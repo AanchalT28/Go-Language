b27e428afe07ade008300ce596f8532fbea01876
AanchalT28 <taanchal734@gmail.com>
2025-02-10 17:41:13 +0530

    Update deferpanicRecover.go

--- DeferPanicRecover/deferpanicRecover.go
+++ DeferPanicRecover/deferpanicRecover.go
@@ -7,7 +7,7 @@ func causePanic() {
 }
 
 func main() {
-	// Defer the recovery logic directly within the main function
+
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered from panic:", r)
@@ -15,6 +15,6 @@ func main() {
 	}()
 
 	fmt.Println("Before panic")
-	causePanic()               // This will trigger a panic
-	fmt.Println("After panic") // This won't be executed
+	causePanic()               //this will trigger a panic
+	fmt.Println("After panic") //this won't be executed
 }
